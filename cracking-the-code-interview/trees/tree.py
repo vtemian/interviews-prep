f0bdb9160c0b35501264b5b75b2a85890368a4bc
Vlad Temian <vladtemian@gmail.com>
2019-03-24 18:17:07 +0200

    Check if a tree is a binary search tree

--- cracking-the-code-interview/trees/tree.py
+++ cracking-the-code-interview/trees/tree.py
@@ -2,13 +2,22 @@ from typing import List
 
 
 class Node:
-    def __init__(self, name: str, nodes: List['Node'] = []):
-        self.name = name
+    def __init__(self, val: str, nodes: List['Node'] = []):
+        self.val = val
         self.nodes = nodes
 
     def __str__(self) -> str:
         nodes = '\n\t'.join([str(node) for node in self.nodes])
-        return "{}\n\t{}".format(self.name, nodes)
+        return "{}\n\t{}".format(self.val, nodes)
+
+    def __eq__(self, other: 'Node') -> bool:
+        return self.val == other.val
+
+    def __lt__(self, other: 'Node') -> bool:
+        return self.val < other.val
+
+    def __gt__(self, other: 'Node') -> bool:
+        return self.val > other.val
 
 
 class Tree:
@@ -18,7 +27,7 @@ class Tree:
     @property
     def is_binary_tree(self) -> bool:
         def check(node: Node) -> bool:
-            if not node.nodes:
+            if not node or not node.nodes:
                 return True
 
             if len(node.nodes) > 2:
@@ -45,6 +54,9 @@ class Tree:
             )
         """
 
+        if nodes is None:
+            return None
+
         if isinstance(nodes, int):
             return Node(nodes)
 
@@ -59,15 +71,40 @@ class Tree:
         return str(self.root)
 
 
+class BinarySearchTree(Tree):
+    @property
+    def is_binary_search_tree(self) -> bool:
+        def check(node: Node) -> bool:
+            if node is None:
+                return True
+
+            if not node.nodes:
+                return True
+
+            left, right = node.nodes
+            if left is None:
+                return not node > right and check(right)
+
+            if right is None:
+                return not node < left and check(left)
+
+            if not left < node < right:
+                return False
+
+            return check(left) and check(right)
+
+        return self.is_binary_tree and check(self.root)
+
+
 # check for binary tree
-for use_case, expected_result in [
+for use_case, *expected_result in [
         (
-            (1,
-             [(2,
-               [3, 4]),
+            (8,
+             [(4,
+               [2, 6]),
               (10,
-               [11, 12])]),
-            True
+               [None, 20])]),
+            True, True
         ),
         (
             (1,
@@ -75,9 +112,15 @@ for use_case, expected_result in [
                [3, 4, 5, 6]),
               (10,
                [11])]),
-            False
+            False, False
         ),
 ]:
-    tree = Tree(Tree.build(use_case))
-    result = tree.is_binary_tree
-    assert result == expected_result, "{} != {}".format(result, expected_result)
+    tree = BinarySearchTree(BinarySearchTree.build(use_case))
+
+    is_binary_tree, is_binary_search_tree = expected_result
+
+    assert tree.is_binary_tree == is_binary_tree, \
+           "{} != {}".format(tree.is_binary_tree, is_binary_tree)
+
+    assert tree.is_binary_search_tree == is_binary_search_tree, \
+           "{} != {}".format(tree, is_binary_search_tree)
