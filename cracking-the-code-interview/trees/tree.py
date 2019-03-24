8fc077119f9510853b624664af35c01adb3f8427
Vlad Temian <vladtemian@gmail.com>
2019-03-24 20:31:51 +0200

    Implemented and tests find and insert for BT and BST

--- cracking-the-code-interview/trees/tree.py
+++ cracking-the-code-interview/trees/tree.py
@@ -1,14 +1,22 @@
 from typing import List
 
+# describe a simple tree
+RAW_NODES = (
+    8,
+    [(4,
+      [2, 6]),
+     (10,
+      [None, 20])]
+)
+
 
 class Node:
-    def __init__(self, val: str, nodes: List['Node'] = []):
+    def __init__(self, val: str, nodes: List['Node'] = None):
         self.val = val
-        self.nodes = nodes
+        self.nodes = nodes or []
 
     def __str__(self) -> str:
-        nodes = '\n\t'.join([str(node) for node in self.nodes])
-        return "{}\n\t{}".format(self.val, nodes)
+        return str(self.val)
 
     def __eq__(self, other: 'Node') -> bool:
         return self.val == other.val
@@ -24,6 +32,73 @@ class Tree:
     def __init__(self, root: Node):
         self.root = root
 
+    def find(self, val: int) -> bool:
+        def _find(node: Node):
+            if node is None:
+                return False
+
+            if node.val == val:
+                return True
+
+            result = False
+            for kid in node.nodes:
+                result |= _find(kid)
+
+            return result
+
+        return _find(self.root)
+
+    @staticmethod
+    def build(nodes: List):
+        """
+            (1,
+                [(2,
+                    [3, 4, 5]
+                 ),
+                 (10,
+                    [11, 12, 13]
+                 ),
+                 (8, )]
+            )
+        """
+
+        if nodes is None:
+            return None
+
+        if isinstance(nodes, int):
+            return Node(nodes)
+
+        root, kids = nodes
+        kids = [
+            Tree.build(kid)
+            for kid in kids
+        ]
+        return Node(root, kids)
+
+    def insert(self, val: int) -> bool:
+        self.root.nodes.append(Node(val))
+        return True
+
+    def __str__(self) -> str:
+        def _repr(node: Node, depth: int = 0) -> str:
+            if not node:
+                return ''
+
+            output = '\n' + '|  ' * depth
+            if depth == 0:
+                output += '-- ' + str(node)
+            else:
+                output += '|-- ' + str(node)
+
+            for kid in node.nodes:
+                output += _repr(kid, depth + 1)
+
+            return output
+
+        return _repr(self.root)
+
+
+class BinaryTree(Tree):
     @property
     def is_binary_tree(self) -> bool:
         def check(node: Node) -> bool:
@@ -41,37 +116,28 @@ class Tree:
 
         return check(self.root)
 
-    @staticmethod
-    def build(nodes: List):
-        """
-            (1,
-                [(2,
-                    [3, 4, 5]
-                 ),
-                 (10,
-                    [11, 12, 13]
-                )]
-            )
-        """
+    def insert(self, val: int) -> bool:
+        if self.find(val):
+            return False
 
-        if nodes is None:
-            return None
+        def _insert(node: Node):
+            if node is None:
+                return False
 
-        if isinstance(nodes, int):
-            return Node(nodes)
+            if len(node.nodes) < 2:
+                node.nodes.append(Node(val))
+                return True
 
-        root, kids = nodes
-        kids = [
-            Tree.build(kid)
-            for kid in kids
-        ]
-        return Node(root, kids)
+            for kid in node.nodes:
+                if _insert(kid):
+                    return True
 
-    def __str__(self) -> str:
-        return str(self.root)
+            return False
+
+        return _insert(self.root)
 
 
-class BinarySearchTree(Tree):
+class BinarySearchTree(BinaryTree):
     @property
     def is_binary_search_tree(self) -> bool:
         def check(node: Node) -> bool:
@@ -96,14 +162,10 @@ class BinarySearchTree(Tree):
         return self.is_binary_tree and check(self.root)
 
 
-# check for binary tree
+# test basic structures
 for use_case, *expected_result in [
         (
-            (8,
-             [(4,
-               [2, 6]),
-              (10,
-               [None, 20])]),
+            RAW_NODES,
             True, True
         ),
         (
@@ -124,3 +186,68 @@ for use_case, *expected_result in [
 
     assert tree.is_binary_search_tree == is_binary_search_tree, \
            "{} != {}".format(tree, is_binary_search_tree)
+
+
+# test find method
+bst = BinarySearchTree(BinarySearchTree.build(RAW_NODES))
+for use_case, expected_result in [
+        (8, True),
+        (20, True),
+        (-1, False),
+]:
+    result = bst.find(use_case)
+    assert result == expected_result, "{} != {}".format(result, expected_result)
+
+
+# test insert method
+bt = BinaryTree(BinaryTree.build(RAW_NODES))
+for use_case, *expected_result in [
+        (1, True, True, """
+-- 8
+|  |-- 4
+|  |  |-- 2
+|  |  |  |-- 1
+|  |  |-- 6
+|  |-- 10
+|  |  |-- 20"""),
+
+        (0, True, True, """
+-- 8
+|  |-- 4
+|  |  |-- 2
+|  |  |  |-- 1
+|  |  |  |-- 0
+|  |  |-- 6
+|  |-- 10
+|  |  |-- 20"""),
+
+        (3, True, True, """
+-- 8
+|  |-- 4
+|  |  |-- 2
+|  |  |  |-- 1
+|  |  |  |  |-- 3
+|  |  |  |-- 0
+|  |  |-- 6
+|  |-- 10
+|  |  |-- 20"""),
+        (8, False, True, """
+-- 8
+|  |-- 4
+|  |  |-- 2
+|  |  |  |-- 1
+|  |  |  |  |-- 3
+|  |  |  |-- 0
+|  |  |-- 6
+|  |-- 10
+|  |  |-- 20"""),
+]:
+    insert_result, find_result, tree_repr = expected_result
+
+    result = bt.insert(use_case)
+    assert result == insert_result, "{} != {}".format(result, insert_result)
+
+    result = bt.find(use_case)
+    assert result == find_result, "{} != {}".format(result, find_result)
+
+    assert str(bt) == tree_repr, "{} != {}".format(str(bt), tree_repr)
