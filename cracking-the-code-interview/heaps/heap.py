2cca44a04c67abad0177244d0b304416e7bf1186
Vlad Temian <vladtemian@gmail.com>
2019-03-28 19:10:51 +0200

    closes #10 closes #11: Implement max and min heap

--- cracking-the-code-interview/heaps/heap.py
+++ cracking-the-code-interview/heaps/heap.py
@@ -20,8 +20,51 @@ class Heap:
 
         return self.store[0]
 
+    def extract(self) -> Union[int, None]:
+        if self.peek is None:
+            return None
+
+        item = self.store[0]
+
+        if len(self.store) == 1:
+            return item
+
+        self.store[0] = self.store[-1]
+        self.store = self.store[:-1]
+
+        self._swap_down()
+
+        return item
+
     def compare(self, current: int, parent: int) -> bool:
         raise NotImplementedError()
+
+    def _swap_down(self):
+        idx = 0
+
+        """
+        0 1 2 3 4 5 6 7 8
+        """
+
+        while 2 * idx < len(self.store):
+            left = right = None
+
+            if 2 * idx + 1 < len(self.store):
+                left = self.store[2 * idx + 1]
+            if 2 * idx + 2 < len(self.store):
+                right = self.store[2 * idx + 2]
+
+            if left is None and right is None:
+                return True
+
+            if left and self.compare(left, self.store[idx]):
+                self.store[idx], self.store[2 * idx + 1] = self.store[2 * idx + 1], self.store[idx]
+                idx = 2 * idx + 1
+            elif right and self.compare(right, self.store[idx]):
+                self.store[idx], self.store[2 * idx + 2] = self.store[2 * idx + 2], self.store[idx]
+                idx = 2 * idx + 2
+            else:
+                return True
 
     def _swap_up(self):
         idx = len(self.store) - 1
@@ -50,8 +93,8 @@ class MaxHeap(Heap):
 
 
 for use_case, expected_result in [
-        ((MinHeap, 4, [4, 1, 2, 3]), 1),
-        ((MaxHeap, 4, [0, 1, 2, 3]), 3),
+        ((MinHeap, 4, [4, 1, 2, 3]), [1, 2, 3, 4]),
+        ((MaxHeap, 4, [0, 1, 2, 3]), [3, 2, 1, 0]),
 ]:
     heap_cls, size, elements = use_case
     heap = heap_cls(size)
@@ -59,4 +102,5 @@ for use_case, expected_result in [
     for element in elements:
         heap.insert(element)
 
-    assert heap.peek == expected_result, "{} != {}".format(heap.peek, expected_result)
+    sorted_items = [heap.extract() for _ in range(size)]
+    assert sorted_items == expected_result, "{} != {}".format(sorted_items, expected_result)
