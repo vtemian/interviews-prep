93515df3b72ec84265be377ce618d3ad9a045b14
Vlad Temian <vladtemian@gmail.com>
2019-05-01 18:00:21 +0300

    leetcode: fix rotated digits

--- leetcode/easy/rotated-digits.py
+++ leetcode/easy/rotated-digits.py
@@ -1,34 +1,17 @@
 class Solution:
     def rotatedDigits(self, N: 'int') -> 'int':
-        def is_rotated(N):
-            rotated_digits = {
-                2: 5,
-                5: 2,
-                6: 9,
-                9: 6
-            }
+        result = 0
 
-            old_number = N
-            new_number = 0
+        for nr in range(1, N + 1):
+            ok = False
 
-            while N:
-                new_number = new_number * 10 + rotated_digits.get(N % 10, N % 10)
-                N //= 10
+            for digit in str(nr):
+                if digit in '347':
+                    break
 
-            rotated_number = 0
-            while new_number:
-                rotated_number = rotated_number * 10 + new_number % 10
-                new_number //= 10
+                if digit in '6952':
+                    ok = True
+            else:
+                result += int(ok)
 
-            return rotated_number != old_number
-
-        result = [
-            n for n in range(N + 1)
-            if is_rotated(n)
-        ]
-        print(result)
-        return len(result)
-
-
-result = Solution().rotatedDigits(10)
-print(result)
+        return result
